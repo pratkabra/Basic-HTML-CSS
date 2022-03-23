7ee78d097b89591e1627d64d08ba3fa9a2b85096
Prateek Kabra <54915285+pratkabra@users.noreply.github.com>
2022-03-23 15:00:51 +0530

    Delete exercise-4.js

--- js-events-assignments/exercise-4.js
+++
@@ -1,6 +0,0 @@
-const userInput = document.querySelector('#user-input')
-
-userInput.addEventListener("input", (event) => {
-    let inputLength = userInput.value.length;
-    document.getElementById("noOfChars").innerHTML = `The above text has ${inputLength} characters`;
-});
