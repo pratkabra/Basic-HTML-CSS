f9e1f558f6a794437e5af860b14b4c67f0b4e451
Prateek Kabra <54915285+pratkabra@users.noreply.github.com>
2022-03-23 15:01:05 +0530

    Delete exercise-5.js

--- js-events-assignments/exercise-5.js
+++
@@ -1,6 +0,0 @@
-const hexInput = document.querySelector('#hex-input')
-
-hexInput.addEventListener("input", (event) => {
-    let hexCode = hexInput.value;
-    document.getElementById("color-box").style.backgroundColor = hexCode;
-});
